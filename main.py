f49fda1175336a172b5e91eeb3244287e0744595
Muhammad Harits R <harits.muhammad.only@gmail.com>
2024-08-07 11:01:08 +0700

    download xml file from sftp server working properly, next try to parse xml file and store it in airtable.

--- main.py
+++ main.py
@@ -1,43 +1,90 @@
 import paramiko
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
-def main(argv=None):
+def connect_sftp(hostname, username, password):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname, username=username, password=password, look_for_keys=False)
+    return ssh.open_sftp()
+
+
+def list_directory(sftp, remote_path):
     try:
-        with paramiko.SSHClient() as ssh:
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.load_system_host_keys()
-            ssh.connect(os.getenv('MC_HOST'), username=os.getenv('MC_USER'), password=os.getenv('MC_PASS'))
+        files = sftp.listdir(remote_path)
+        print(f"Contents of {remote_path}:")
+        for file in files:
+            try:
+                file_attr = sftp.stat(os.path.join(remote_path, file))
+                file_type = 'D' if file_attr.st_mode & 0o40000 else 'F'
+                print(f"{file_type} - {file}")
+            except IOError:
+                print(f"? - {file} (unable to get file information)")
+    except IOError as e:
+        print(f"Error listing directory {remote_path}: {str(e)}")
 
-            with ssh.open_sftp() as sftp:
-                # remote_dir = 'public'  # Directory you want to browse
-                local_dir = 'D:/Naru/morris_sftp/'  # Local directory to save files
 
-                # Ensure local directory exists
-                if not os.path.exists(local_dir):
-                    os.makedirs(local_dir)
+def download_file(sftp, remote_path, local_path):
+    try:
+        sftp.get(remote_path, local_path)
+        print(f"File downloaded successfully to {local_path}")
+    except IOError as e:
+        print(f"Error downloading file: {str(e)}")
 
-                # List files in remote directory
-                remote_files = sftp.listdir()
-                print(remote_files)
-                #
-                # print(f"Files in remote directory '{remote_dir}': {remote_files}")
-                #
-                # # Download each file
-                # for file in remote_files:
-                #     remote_file_path = os.path.join(remote_dir, file)
-                #     local_file_path = os.path.join(local_dir, file)
-                #
-                #     try:
-                #         sftp.get(remote_file_path, local_file_path)
-                #         print(f"Downloaded {remote_file_path} to {local_file_path}")
-                #     except Exception as e:
-                #         print(f"Failed to download {remote_file_path}: {e}")
+
+def get_latest_files(sftp, remote_path):
+    files = sftp.listdir(remote_path)
+
+    full_product_files = [f for f in files if f.startswith('AvailableBatch_Full_Product_Data_')]
+    inventory_files = [f for f in files if f.startswith('AvailableBatch_Inventory_Only_')]
+
+    if full_product_files:
+        latest_full_product = max(full_product_files,
+                                  key=lambda x: datetime.strptime('_'.join(x.split('_')[4:6]).split('.')[0], '%Y%m%d_%H%M%S'))
+    else:
+        latest_full_product = None
+
+    if inventory_files:
+        latest_inventory = max(inventory_files,
+                               key=lambda x: datetime.strptime('_'.join(x.split('_')[3:5]).split('.')[0], '%Y%m%d_%H%M%S'))
+    else:
+        latest_inventory = None
+
+    return latest_full_product, latest_inventory
+
+
+def main():
+    hostname = os.getenv('MC_HOST')
+    username = os.getenv('MC_USER')
+    password = os.getenv('MC_PASS')
+    remote_path = '/'
+    local_path = 'D:/Naru/morris_sftp/'
+    sftp = None
+
+    try:
+        sftp = connect_sftp(hostname, username, password)
+
+        latest_full_product, latest_inventory = get_latest_files(sftp, remote_path)
+
+        if latest_full_product:
+            download_file(sftp, os.path.join(remote_path, latest_full_product), os.path.join(local_path, latest_full_product))
+        else:
+            print("No AvailableBatch_Full_Product_Data file found")
+
+        if latest_inventory:
+            download_file(sftp, os.path.join(remote_path, latest_inventory), os.path.join(local_path, latest_inventory))
+        else:
+            print("No AvailableBatch_Inventory_Only file found")
 
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred: {str(e)}")
+    finally:
+        if sftp:
+            sftp.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
