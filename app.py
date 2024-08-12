da837255c20144ec77adb1c9ad2dd5d06119b62b
Muhammad Harits R <harits.muhammad.only@gmail.com>
2024-08-12 15:09:02 +0700

    skip airtable, Create UI for upsert product from morris to shopify.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import all class from tkinter
-from os import access
+import os
 from tkinter import *
 
 # import filedialog method from tkinter
@@ -9,10 +9,13 @@ from tkinter import filedialog
 def browse_file():
     filename = filedialog.askopenfilename(initialdir='./',
         title='Select file to upload',
-        filetypes=[('Csv file', '*.csv'), ("All files","*.*")]
+        filetypes=[('Csv file', '*.csv'), ("All files", "*.*")]
         )
 
     import_file_entry.insert(END, filename)
+
+def ok_button():
+    pass
 
 
 # UI Build
@@ -20,37 +23,52 @@ window = Tk()
 
 # Window Config
 window.title('Shopify Uploader')
-window.geometry('700x200')
+window.geometry('670x280')
 window.config(bg='light grey', padx=15, pady=15)
 
 
 # Store name input
-store_name_label = Label(window, text='Store Name: ', width=13)
-store_name_label.grid(column=0, row=0)
+store_name_label = Label(window, text='Store Name: ', bg='light grey')
+store_name_label.grid(column=0, row=0, pady=10, sticky='NW', padx=(0, 0))
 
-store_name_entry = Entry(window)
-store_name_entry.grid(column=1, row=0,)
+store_name_entry = Entry(window, width=30)
+store_name_entry.grid(column=1, row=0)
 
 
 
 # access token input
-access_token_label = Label(window, text='Access Token: ', width=13)
-access_token_label.grid(column=2, row=0)
+access_token_label = Label(window, text='Access Token: ', bg='light grey')
+access_token_label.grid(column=2, row=0, padx=(15, 0))
 
-access_token_entry = Entry(window)
+access_token_entry = Entry(window, width=45)
 access_token_entry.grid(column=3, row=0)
 
 
 # Import File
-import_file_label = Label(window, text='Import file: ')
-import_file_label.grid(column=0, row=1, padx=15)
+import_file_label = Label(window, text='Import file: ', bg='light grey')
+import_file_label.grid(column=0, row=1, pady=10, sticky='NW')
 
-import_file_entry = Entry(window)
-import_file_entry.configure(width=65)
-import_file_entry.grid(column=0, row=2, columnspan=3)
+import_file_entry = Entry(window, width=80)
+import_file_entry.grid(column=1, row=1, columnspan=3, sticky='W')
 
-import_file_button = Button(window, text='Browse', command=browse_file)
-import_file_button.grid(column=3, row=2)
+import_file_button = Button(window, text='Browse', command=browse_file, bg='light grey')
+import_file_button.grid(column=3, row=1, sticky='E')
+
+
+# Ok Button
+check_img = PhotoImage(file='asset/vecteezy_check-mark-icon_15130843.png')
+ok_button_img = check_img.subsample(400, 400)
+ok_button = Button(window,
+                   text='Ok',
+                   image=ok_button_img,
+                   compound='left',
+                   bg='light grey',
+                   width=41,
+                   height=20,
+                   command=ok_button)
+ok_button.grid(column=3, row=3, sticky='SE', pady=(135, 15))
+
+
 
 
 window.mainloop()
