4fe4f4a1555322062f175b5d7a8edf9c9472d551
harits <harits.muhammad.only@gmail.com>
2024-08-14 05:20:50 +0700

    Build converter.py handle image src field

--- app/converter.py
+++ app/converter.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def to_handle(title):
     if pd.isna(title):
-        return None
+        return ''
     title.replace('-', '')
     pattern = re.compile(r"\b[a-zA-Z0-9]+\b")
     matches = pattern.findall(title.lower().strip())
@@ -22,17 +22,39 @@ def generate_category(*args):
 
 
 def to_tags(theme):
-    if pd.isna(theme):
-        return ''
+    if (theme == '') | pd.isna(theme):
+        result = ''
+    else:
+        result = theme.replace(';', ',')
 
-    return theme.replace(';', ',')
+    return result
+
+def generate_image(image_urls):
+    print(image_urls)
+    if (len(image_urls) == 0) | (pd.isna(image_urls)):
+        result = ''
+    else:
+        result = ';'.join(image_urls)
+
+    return result
+
+def to_alt_text(image_url):
+    if (image_url == '') | (pd.isna(image_url)):
+        result = ''
+    else:
+        result = image_url.split('/')[-1].split('.')[0]
+    print(result)
+
+    return result
 
 
 def to_shopify(morris_file_path):
-    shopify_df = pd.read_csv('../asset/products_export_10.csv', nrows=0)
     morris_df = pd.read_excel(morris_file_path)
-    print(shopify_df)
+    shopify_df = pd.read_csv('./data/products_export_10.csv', nrows=1)
     print(morris_df.columns)
+    print(shopify_df.columns)
+    print(shopify_df['Variant Requires Shipping'])
+    shopify_df = pd.DataFrame()
     shopify_df['Handle'] = morris_df['ProductName'].apply(to_handle)
     shopify_df['Title'] = morris_df['FormattedName']
     shopify_df['Body (HTML)'] = morris_df['FullDescription']
@@ -40,24 +62,42 @@ def to_shopify(morris_file_path):
     shopify_df['Product Category'] = morris_df.apply(lambda x: generate_category((x['PrimaryCategory'],
                                                                                   x['SecondaryCategory'],
                                                                                   x['ThirdCategory'])), axis=1)
-    shopify_df['Type'] = 'Customes',
-    shopify_df['Tags'] = morris_df['Theme'].apply(to_tags),
-    shopify_df.loc['Published'] = True,
-    shopify_df['Option1 Name'] = morris_df['VariantType1']
-    shopify_df['Option1 Value'] = morris_df['VariantValue1']
+    shopify_df['Type'] = 'Costumes'
+    shopify_df['Tags'] = morris_df['Theme'].apply(to_tags)
+    shopify_df['Published'] = True
+    shopify_df['Option1 Name'] = morris_df['VariationType1']
+    shopify_df['Option1 Value'] = morris_df['VariationValue1']
     shopify_df['Option1 Linked To'] = ''
-    shopify_df['Option2 Name'] = morris_df['VariantType2']
-    shopify_df['Option2 Value'] = morris_df['VariantValue2']
+    shopify_df['Option2 Name'] = morris_df['VariationType2']
+    shopify_df['Option2 Value'] = morris_df['VariationValue2']
     shopify_df['Option2 Linked To'] = ''
-    shopify_df['Option3 Name'] = morris_df['VariantType3']
-    shopify_df['Option3 Value'] = morris_df['VariantValue2']
+    shopify_df['Option3 Name'] = ''
+    shopify_df['Option3 Value'] = ''
     shopify_df['Option3 Linked To'] = ''
     shopify_df['Variant SKU'] = morris_df['Sku']
-    shopify_df['Variant Grams'] = morris_df['itemWeight']
+    shopify_df['Variant Grams'] = morris_df['ItemWeight']
     shopify_df['Variant Inventory Tracker'] = 'shopify'
+    shopify_df['Variant Inventory Qty'] = 0
+    shopify_df['Variant Inventory Policy'] = 'deny'
+    shopify_df['Variant Inventory Fulfillment Service'] = 'manual'
+    shopify_df['Variant Price'] = morris_df['Price']
+    shopify_df['Variant Compare At Price'] = ''
+    shopify_df['Variant Requires Shipping'] = True
+    shopify_df['Variant Taxable'] = True
+    shopify_df['Variant Barcode'] = morris_df['Selling Unit Master UPC']
+    shopify_df['Image Src'] = morris_df.apply(lambda x: generate_image((x['PrimaryImgLink'],
+                                                                        x['ImgAlternate1'],
+                                                                        x['ImgAlternate2'],
+                                                                        x['ImgAlternate3'],
+                                                                        x['ImgAlternate4'],
+                                                                        x['ImgAlternate5'],
+                                                                        x['ImgAlternate6'])), axis=1)
+    shopify_df['Image Position'] = 1
+    shopify_df['Image Alt Text'] = morris_df['PrimaryImgLink']
+
 
     print(shopify_df)
 
 
 if __name__ == '__main__':
-    to_shopify('../data/All_Products_PWHSL.xlsx')
+    to_shopify('./data/All_Products_PWHSL.xlsx')
