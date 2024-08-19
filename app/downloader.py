fa4e38f49a8a7a670c7648b944992d49d5408a61
harits <harits.muhammad.only@gmail.com>
2024-08-20 05:35:50 +0700

    ADD: Fetch from dataframe, TODO: fix download function

--- app/downloader.py
+++ app/downloader.py
@@ -1,9 +1,13 @@
+import ast
 import os
 from urllib.parse import unquote, quote
 from httpx import Client, HTTPError
+import pandas as pd
 
-def fetch(url, proxy=None, output_dir='data'):
+def fetch(url, proxy=None, output_dir='data/images', filename=''):
+    encoded_url = quote(url, safe=':/?&=')
     headers = {
+        'Host': 's7.orientaltrading.com',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
         'Accept-Encoding': 'gzip, deflate, br, zstd',
         'Accept-Language': 'en-US,en;q=0.5',
@@ -22,13 +26,13 @@ def fetch(url, proxy=None, output_dir='data'):
     os.makedirs(output_dir, exist_ok=True)
 
     # Extract filename from URL and sanitize it
-    filename = unquote(url.split("/")[-1])
-    filename = "".join([c for c in filename if c.isalnum() or c in (' ', '-', '_')]).rstrip()
+    # filename = unquote(url.split("/")[-1])
+    # filename = "".join([c for c in filename if c.isalnum() or c in (' ', '-', '_')]).rstrip()
     filepath = os.path.join(output_dir, f"{filename}.jpeg")
 
     try:
         with Client(headers=headers, proxies={'http://': proxy, 'https://': proxy} if proxy else None, timeout=30) as client:
-            response = client.get(url)
+            response = client.get(encoded_url)
             response.raise_for_status()
 
             with open(filepath, 'wb') as file:
@@ -42,13 +46,21 @@ def fetch(url, proxy=None, output_dir='data'):
 
     return None
 
+def extract_url(x):
+    image_urls = ast.literal_eval(x)
+    image_url = image_urls[0]
+
+    return image_url
+
 if __name__ == '__main__':
-    url = 'https://s7.orientaltrading.com/is/image/OrientalTrading/VIEWER_ZOOM/10  skeleton gargoyle decoration~sew81858'
-    encoded_url = quote(url, safe=':/?&=')
-    response = fetch(encoded_url,
+    df = pd.read_csv('./data/create_products.csv')[10:13]
+    print(df)
+    df['url'] = df['Image Src'].apply(extract_url)
+    df.apply(lambda x: fetch(x['url'], filename=x['Handle']), axis=1)
+    # response = fetch(url, filename=handle,
         # proxy='http://47.251.70.179:80'
-    )
-    if response:
-        print(f"Image downloaded successfully: {response}")
-    else:
-        print("Failed to download image")
+    # )
+    # if response:
+    #     print(f"Image downloaded successfully: {response}")
+    # else:
+    #     print("Failed to download image")
