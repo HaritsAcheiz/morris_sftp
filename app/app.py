bbcf6e8df13142980f1476a4497762fb49453cc6
harits <harits.muhammad.only@gmail.com>
2024-09-09 03:13:35 +0700

    change chunked df range to 4

--- app/app.py
+++ app/app.py
@@ -94,14 +94,8 @@ def import_button():
     # upload_and_get_link()
 
     # ====================================Handle limit with chunked data==============================================
-<<<<<<< HEAD
-    chunked_df = chunk_data('data/create_products.csv', nrows=100)
-    for create_df in chunked_df[0:1]:
-=======
     chunked_df = chunk_data('data/create_products.csv', nrows=249)
-    print(f'Data length : {len(chunked_df)}')
-    for create_df in chunked_df[11:12]: # not executed yet caused by daily limit
->>>>>>> 63b6f2ffea2cdc55d0e1a2cf33780e61ad5cbd1d
+    for create_df in chunked_df[0:4]:
 
         # =======================================Merge create product with image =========================================
         image_df = pd.read_csv('data/product_images.csv')
@@ -137,6 +131,7 @@ def import_button():
         created = False
         while not created:
             created = import_status(client)
+    print(f'Data length : {len(chunked_df)}')
 
     # =======================================Merge update product with image =========================================
     # merge_images(update_df, image_df)
